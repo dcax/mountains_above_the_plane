4c02964ffb92fcdf15681bb0c9642720fe1a3745
dylanalexander <dylan@postriver.com>
2019-02-16 13:22:31 -0600

    Added code to read from file and triangulate

--- mk_tri.py
+++ mk_tri.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt #REMOVE THIS AT RUN; Unneccessay
 
 import pandas as pd
 import numpy as np
+import time
 from pprint import pprint
 #Uses Delaunay triangulation for efficiency
 #and optimal triangle choice
@@ -20,20 +21,28 @@ locs = pd.read_csv(input_csv_file,delimiter=',')
 #Name col is not used at the moment
 print(locs)
 
+rand = lambda: np.random.random()*10000 - 5000
+
 points = locs[['x','y']].values #np.asarray(locs[['x','y']]) 
 #turns pandas "spreadsheet" into array of numpy x,y tuples
-points = np.array([[0, 0], [0, 1.1], [1, 0], [1, 1]])
-
-
+#points = np.array([[0, 0], [0, 1.1], [1, 0], [1, 1]])
+points = []
+for i in range(150):
+    points.append([rand(), rand()])
+points = np.asarray(points)
 pprint(points)
 from scipy.spatial import Delaunay 
+start_time = time.time()
+
 triangulation = Delaunay(points)
+print(time.time()-start_time)
 print("Tri created.")
+print(triangulation.simplices.copy())
 
 
 plt.triplot(points[:,0],points[:,1], triangulation.simplices.copy())
 plt.plot(points[:,0],points[:,1], 'o')
-plt.show()
+plt.show(block=True)
 
 
 
